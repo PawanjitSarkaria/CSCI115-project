56218f55e64e6d26bd6d7f142f4e1719bdd2ee07
PawanjitSarkaria <139204367+PawanjitSarkaria@users.noreply.github.com>
2025-03-20 10:39:04 -0700

    Add files via upload

---
+++ Project/Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project
+//
+//  Created by Pawanjit Sarkaria on 3/20/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout<<"this is the project file"<<std::endl;
+    return 0;
+}
